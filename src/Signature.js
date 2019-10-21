d5da1927785274f25ccd960a2880b031ec9eabf5
Unknown <stijn.rooselaers@student.ehb.be>
2019-10-21 10:46:10 +0200

    Removed BeforeAll

--- src/Signature.js
+++ src/Signature.js
@@ -1,18 +1,30 @@
 const _sodium = require('libsodium-wrappers');
-let theKey;
 
 
 
-beforeAll(async() => {
+
+
+async function ready() {
     await _sodium.ready;
-    theKey = _sodium.crypto_sign_keypair();
-});
+}
+
+let publicKey, privateKey;
 
 exports.sign = async function(msg){
-    return _sodium.crypto_sign(msg, theKey.privateKey);
+    await ready()
+    return _sodium.crypto_sign(msg, privateKey)
 }
 
 exports.verifyingKey = async function(){
-    return theKey.publicKey;
-}
+    await ready()
+  
+    if (privateKey == null) {
+        let keyPair = _sodium.crypto_sign_keypair()
+
+        privateKey = keyPair.privateKey
+        publicKey = keyPair.publicKey
+    }
+  
+
+    return publicKey}
 
